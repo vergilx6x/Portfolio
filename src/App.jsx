ff4470c2d672a1ea9a6133d91449f7d367220a85
Mohamed Amine Thami <136102816+vergilx6x@users.noreply.github.com>
2024-07-02 14:24:05 +0000

    Editing the home page

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Mohamed Amine Thami",
+  title: "Software Enginner",
+  email: "med.work96@hotmail.com",
+  gitHub: "vergilx6x",
+  instagram: "vergilx6x",
+  linkedIn: "Mohamed Amine Thami",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "vergil6X",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
